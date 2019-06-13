30be1a17f9a36295fa29b22630d2c150e89123c5
s-shumakov <ssshumakov@gmail.com>
2019-06-14 01:54:28 +0300

    Change context configuration, add localization.

--- HW_01-student-testing/src/main/java/ru/otus/spring01/service/QuestionsReaderServiceImpl.java
+++ HW_01-student-testing/src/main/java/ru/otus/spring01/service/QuestionsReaderServiceImpl.java
@@ -1,24 +1,29 @@
 package ru.otus.spring01.service;
 
 import com.opencsv.bean.CsvToBeanBuilder;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.ClassPathResource;
+import org.springframework.stereotype.Service;
 import ru.otus.spring01.domain.CsvQuestion;
 
 import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
 
+@Service
 public class QuestionsReaderServiceImpl implements QuestionsReaderService {
+    @Value("${questions.file.path}")
     private String path;
-
-    public QuestionsReaderServiceImpl(String path) {
-        this.path = path;
-    }
+    @Value("${locale}")
+    private Locale locale;
 
     @Override
     public List<CsvQuestion> readQuestions() {
         List<CsvQuestion> csvQuestions = new ArrayList<>();
-
+        if(this.locale.getLanguage().equalsIgnoreCase("en")){
+            this.path = this.path.replace(".csv", "_" + locale.getLanguage() + ".csv");
+        }
         try (InputStream resource = new ClassPathResource(path).getInputStream()) {
             BufferedReader reader = new BufferedReader(new InputStreamReader(resource));
             csvQuestions = this.parseToBean(reader);
--- HW_01-student-testing/src/main/java/ru/otus/spring01/service/TestingServiceImpl.java
+++ HW_01-student-testing/src/main/java/ru/otus/spring01/service/TestingServiceImpl.java
@@ -1,27 +1,41 @@
 package ru.otus.spring01.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.MessageSource;
+import org.springframework.stereotype.Service;
 import ru.otus.spring01.domain.CsvQuestion;
 
 import java.util.List;
+import java.util.Locale;
 import java.util.Scanner;
 
+@Service
 public class TestingServiceImpl implements TestingService {
     private QuestionsReaderService questionsReaderService;
+    private MessageSource messageSource;
+    @Value("${answers.correct.number}")
+    private int correctAnswersNumber;
+    @Value("${locale}")
+    private Locale locale;
 
-    public TestingServiceImpl(QuestionsReaderService questionsReaderService) {
+    public TestingServiceImpl(
+            QuestionsReaderService questionsReaderService,
+            MessageSource messageSource) {
         this.questionsReaderService = questionsReaderService;
+        this.messageSource = messageSource;
     }
 
     @Override
     public void runTest() {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Введите фамилию и имя: ");
+        System.out.println(messageSource.getMessage("input.name", null, this.locale));
         String userName = scanner.nextLine();
         int trueAnswers = 0;
         List<CsvQuestion> csvQuestions = this.questionsReaderService.readQuestions();
         for (CsvQuestion csvQuestion : csvQuestions) {
             System.out.println(csvQuestion.getQuestion());
-            System.out.println("Выберите правильный ответ (a, b, c, d):");
+            System.out.println(messageSource.getMessage("choose.answer", null, this.locale));
             System.out.println(csvQuestion.getAnswerA());
             System.out.println(csvQuestion.getAnswerB());
             System.out.println(csvQuestion.getAnswerC());
@@ -31,6 +45,15 @@ public class TestingServiceImpl implements TestingService {
                 trueAnswers++;
             }
         }
-        System.out.println(userName + ", количество правильных ответов: " + trueAnswers + " из " + csvQuestions.size());
+        System.out.println(
+                messageSource.getMessage(
+                        "correct.answers.count", new String[]{
+                                userName, String.valueOf(trueAnswers), String.valueOf(csvQuestions.size())
+                        }, this.locale));
+        if (trueAnswers < correctAnswersNumber) {
+            System.out.println(messageSource.getMessage("test.failed", null, this.locale));
+        } else {
+            System.out.println(messageSource.getMessage("test.passed", null, this.locale));
+        }
     }
 }
