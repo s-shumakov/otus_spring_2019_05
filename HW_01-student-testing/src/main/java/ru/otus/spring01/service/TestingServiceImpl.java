d2d61db02ede1711805c10cdb86977489e236a58
s-shumakov <ssshumakov@gmail.com>
2019-06-17 00:58:18 +0300

    Fix issues.

--- HW_01-student-testing/src/main/java/ru/otus/spring01/service/TestingServiceImpl.java
+++ HW_01-student-testing/src/main/java/ru/otus/spring01/service/TestingServiceImpl.java
@@ -5,14 +5,12 @@ import org.springframework.context.MessageSource;
 import org.springframework.stereotype.Service;
 import ru.otus.spring01.domain.CsvQuestion;
 
-import java.util.List;
-import java.util.Locale;
-import java.util.Scanner;
+import java.util.*;
 
 @Service
 public class TestingServiceImpl implements TestingService {
-    private QuestionsReaderService questionsReaderService;
-    private MessageSource messageSource;
+    private final QuestionsReaderService questionsReaderService;
+    private final MessageSource messageSource;
     @Value("${answers.correct.number}")
     private int correctAnswersNumber;
     @Value("${locale}")
@@ -29,24 +27,32 @@ public class TestingServiceImpl implements TestingService {
     public void runTest() {
         Scanner scanner = new Scanner(System.in);
         System.out.println(messageSource.getMessage("input.name", null, this.locale));
-        String userName = scanner.nextLine();
+        String line = scanner.nextLine().trim();
+        while (line.isEmpty()) {
+            System.out.println(messageSource.getMessage("incorrect.input.name", null, this.locale));
+            line = scanner.nextLine().trim();
+        }
+        String userName = line;
         int trueAnswers = 0;
         List<CsvQuestion> csvQuestions = this.questionsReaderService.readQuestions();
         for (CsvQuestion csvQuestion : csvQuestions) {
             System.out.println(csvQuestion.getQuestion());
             System.out.println(messageSource.getMessage("choose.answer", null, this.locale));
             csvQuestion.getAnswers().values().stream().sorted().forEach(System.out::println);
-            String answer = scanner.nextLine();
+            while (!scanner.hasNext("[a-dA-D]")) {
+                System.out.println(messageSource.getMessage("incorrect.input.answer", null, this.locale));
+                scanner.next();
+            }
+            String answer = scanner.next();
             System.out.println();
             if (answer.equals(csvQuestion.getTrueAnswer())) {
                 trueAnswers++;
             }
         }
         System.out.println(
-                messageSource.getMessage(
-                        "correct.answers.count", new String[]{
-                                userName, String.valueOf(trueAnswers), String.valueOf(csvQuestions.size())
-                        }, this.locale));
+                messageSource.getMessage("correct.answers.count", new String[]{
+                        userName, String.valueOf(trueAnswers), String.valueOf(csvQuestions.size())
+                }, this.locale));
         if (trueAnswers < correctAnswersNumber) {
             System.out.println(messageSource.getMessage("test.failed", null, this.locale));
         } else {
